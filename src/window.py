6ca9d9e43b61b62de392715613898bf98053258e
Andras Molnar <molnarandris@gmail.com>
2025-05-15 19:03:39 +0200

    Added GtkPaned

--- src/window.py
+++ src/window.py
@@ -24,7 +24,5 @@ from gi.repository import Gtk
 class TexwriterWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'TexwriterWindow'
 
-    label = Gtk.Template.Child()
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
