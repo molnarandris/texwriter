577e57550c66580da5cf9fce1bfd8726d9b1aa41
Andras Molnar <molnarandris@gmail.com>
2025-06-03 08:24:20 +0200

    feat: add ability to run command on host

--- src/asyncio.py
+++ src/asyncio.py
@@ -16,3 +16,23 @@ def create_task(awaitable):
 	task.add_done_callback(task_done)
 	return task
 
+async def run_command_on_host(cmd):
+    cmd = ["flatpak-spawn", "--host"] + cmd
+    process = await asyncio.create_subprocess_exec(
+        *cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    try:
+        stdout, stderr = await process.communicate()
+    except asyncio.CancelledError:
+        process.terminate()
+        raise
+
+    if process.returncode!=0:
+        if stderr.decode().startswith("Portal call failed:"):
+            raise ChildProcessError("Flatpak portal call failed")
+        return False, stdout.decode()
+
+    return True, stdout.decode()
+
