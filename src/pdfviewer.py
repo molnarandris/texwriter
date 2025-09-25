408ff9a0c318f7b53e526ce7ee0ddc7c630c8f1f
Andras Molnar <molnarandris@gmail.com>
2025-09-25 19:36:34 +0200

    refactor: Use pymupdf for detecting presence of pdf

--- src/pdfviewer.py
+++ src/pdfviewer.py
@@ -2,7 +2,7 @@ import gi
 from gi.repository import Adw
 from gi.repository import Gtk
 from gi.repository import Gio
-
+import pymupdf
 
 @Gtk.Template(resource_path='/com/github/molnarandris/texwriter/pdfviewer.ui')
 class PdfViewer(Gtk.Widget):
@@ -15,17 +15,20 @@ class PdfViewer(Gtk.Widget):
 
         layout = Gtk.BinLayout()
         self.set_layout_manager(layout)
-        self._file = None
+        self._path = None
 
     def set_path(self,path):
-        self._file = Gio.File.new_for_path(path)
+        self._path = path
         self.reload()
 
     def show_empty(self):
         self.stack.set_visible_child_name("empty")
 
     def reload(self):
-        if self._file.query_exists(None):
-            self.stack.set_visible_child_name("content")
-        else:
+        try:
+            document = pymupdf.open(self._path)
+        except pymupdf.FileNotFoundError:
             self.stack.set_visible_child_name("empty")
+            return
+
+        self.stack.set_visible_child_name("content")
